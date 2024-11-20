353e7ea7369937c8b89f55eb85a630083ff6fcbc
RoseMcc04 <mccormacka@appstate.edu>
2024-11-20 14:45:50 -0500

    pushing stuff

---
+++ HW10_RoseM/scripts/student_comparison.py
@@ -0,0 +1,106 @@
+"""
+Author: Rose McCormack
+Version: 20 November 2024
+Module: student.py
+Desscription: Resources to manage a student's name and test scores.
+"""
+
+class Student(object):
+    """
+    Represents a student and provides accessors and mutators for data access.\n
+    The main attributes being tracked are the student's name and the student's grades.
+
+    Attributes:
+        name (str): The name of the student.
+        scores (list[int]): The most recent grades of the student.
+    """
+
+    def __init__(self, name: str, number: int):
+        """
+        Initializes a Student instance with a name and grades.
+
+        Args:
+            name (str): The name of the student.
+            grades (list[int]): The most recent grades of the student.
+        """
+        self.name = name
+        self.scores = []
+        for count in range(number):
+            self.scores.append(0)
+
+    def getName(self) -> str:
+        """
+        Returns the student's name.
+
+        Return:
+            str: The name of the student.
+        """
+        return self.name
+  
+    def setScore(self, i: int, score: int):
+        """
+        Resets the ith score, counting from 1.
+
+        Args:
+            i (int): The index of the score the user wants to access.
+            score (int): The score that is replacing the initial score being accessed.
+        """
+        self.scores[i - 1] = score
+
+    def getScore(self, i: int) -> int:
+        """
+        Returns the ith score, counting from 1.
+
+        Args:
+            i (int): The index of the score the user wants to retrieve.
+        
+        Returns:
+            int: The specific score being accessed.
+        """
+        return self.scores[i - 1]
+   
+    def getAverage(self) -> float:
+        """
+        Returns the average score.
+
+        Return:
+            float: The average of the scores for a student.
+        """
+        return sum(self.scores) / len(self.scores)
+    
+    def getHighScore(self) -> int:
+        """
+        Returns the highest score.
+
+        Return:
+            int: The maximum score of the student.
+        """
+        return max(self.scores)
+ 
+    def __str__(self) -> str:
+        """
+        Returns the string representation of the student.
+
+        Return:
+            str: The string representation of the student object.
+        """
+        return "Name: " + self.name  + "\nScores: " + \
+               " ".join(map(str, self.scores))
+    
+    def __eq__(self, other) -> bool:
+        """
+        Checks if two students are equal to 
+        """
+
+def main():
+    """A simple test."""
+    student = Student("Ken", 5)
+    print(str(student))
+    for i in range(1, 6):
+        student.setScore(i, 100)
+    print(str(student))
+
+if __name__ == "__main__":
+    main()
+
+
